b27657f5a3343e00d4e8fb550edb7f829981b38c
elisepolezel <elisepolezel@gmail.com>
2013-11-20 20:50:36 -0500

    Added staticpage controller

--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Classboard::Application.routes.draw do
   #   match 'products/:id' => 'catalog#view'
   # Keep in mind you can assign values other than :controller and :action
 
+root :to => "static_pages#index"
+
   # Sample of named route:
   #   match 'products/:id/purchase' => 'catalog#purchase', :as => :purchase
   # This route can be invoked with purchase_url(:id => product.id)
